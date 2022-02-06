f02459c29425b3e187224b114eb287acdd6549c9
Gilad Zur <Gilad.zur90@gmail.com>
2022-02-06 18:44:03 +0200

    second_commit

--- py_mongo.py
+++ py_mongo.py
@@ -8,10 +8,6 @@ mydb = myclient["first_database"]
 mycol = mydb["people"]
 
 if sys.argv[1] == "insert":
-	f=sys.argv[2]
-	ff=sys.argv[3]
-	print("first name :"+f)
-	print("last name :"+ff)
 	mydict = {"firstname":sys.argv[2],"lastname":sys.argv[3]}
 	x=mycol.insert_one(mydict)
 if sys.argv[1] == "show":
